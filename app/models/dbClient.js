168a3f4ebbeccae2134fa00709b3fe21aaa693cd
baptiste-boimard <bouketin27@gmail.com>
2023-07-06 14:16:54 +0200

    Modification config heroku

--- app/models/dbClient.js
+++ app/models/dbClient.js
@@ -8,21 +8,20 @@ const {Pool} = require('pg');
  * @export client
  * @class Pool
  */
-// //Remote connection
-// const client = new Pool({
+//Remote connection
+const client = new Pool({
 
-//   connectionsString: process.env.DATABASE_URL,
-//   ssl : {rejectUnauthorized: false, // Accept the fact that we aren't in SSL security
-//   },
-// });
+  connectionsString: process.env.DATABASE_URL,
+  ssl : {rejectUnauthorized: false, // Accept the fact that we aren't in SSL security
+  },
+});
 
 //Local connection
-const client = new Pool({
-connectionString: DATABASE_URL,
+// const client = new Pool({
 // user: process.env.PG_USER,
 // password: process.env.PG_PASSWORD,
 // database: process.env.PG_DATABASE,
-});
+// });
 
 client.connect();
 
